afed3f8ac1fec38aa940daf46bbdd1913f0d9de6
Niko Lagman <Lagman.Niko@gmail.com>
2017-06-16 03:40:30 -0400

    Set up express app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+import * as logger from 'morgan';
 
 class App {
   public express: express.Application;
 
   constructor () {
     this.express = express();
+    this.services();
+    this.routes();
+  }
+
+  private services () : void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  private routes () : void {
   }
 }
+
+export default new App().express;
